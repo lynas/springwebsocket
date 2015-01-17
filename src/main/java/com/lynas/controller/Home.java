eeb1fd34a38cd4bd590c6f3e0e5db13c4c99d47c
lynas <szlynas@gmail.com>
2015-01-17 17:32:15 +0600

    spring web socket with sockjs and stomp with maven

--- src/main/java/com/lynas/controller/Home.java
+++ src/main/java/com/lynas/controller/Home.java
@@ -1,5 +1,7 @@
 package com.lynas.controller;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,5 +18,13 @@ public class Home {
     public String page1() {
 
         return "page1.jsp";
+    }
+
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(3000); // simulated delay
+        return new Greeting("Hello, " + message.getName() + "!");
     }
 }
--- src/main/java/com/lynas/utils/AppConfig.java
+++ src/main/java/com/lynas/utils/AppConfig.java
@@ -2,6 +2,7 @@ package com.lynas.utils;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -22,5 +23,10 @@ public class AppConfig extends WebMvcConfigurerAdapter {
     public void configureDefaultServletHandling(
             DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter jacksonMessageConverter(){
+        return new MappingJackson2HttpMessageConverter();
     }
 }
